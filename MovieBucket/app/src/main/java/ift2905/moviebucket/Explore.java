e9a41acc3492182df8409b3106057f73e6501f4c
Amelie <ame.lacombe.r@gmail.com>
2017-03-20 12:43:14 -0400

    added drawer to main activity

--- MovieBucket/app/src/main/java/ift2905/moviebucket/Explore.java
+++ MovieBucket/app/src/main/java/ift2905/moviebucket/Explore.java
@@ -1,8 +1,16 @@
 package ift2905.moviebucket;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -14,21 +22,33 @@ import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies;
 import info.movito.themoviedbapi.model.MovieDb;
 
-public class Explore extends AppCompatActivity {
+public class Explore extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    private String[] titles = new String[5];
+    private String[] suggestions = new String[5];
 
-    ListView liste;
+    ListView suggestionsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         //Testing api to display content **to be replaced with proper behavior**
         FetchMovie fetcher = new FetchMovie();
         fetcher.execute();
-        liste = (ListView) findViewById(R.id.list);
+        suggestionsList = (ListView) findViewById(R.id.list);
 
         // TODO: Read suggestions from local data base
         // TODO: Display suggestions on startup
@@ -36,8 +56,64 @@ public class Explore extends AppCompatActivity {
         // TODO: Read text input from user, upon click search and display results
 
         // TODO: On click on list item, create MovieView activity from item
+    }
 
-        // TODO: Add navigation drawer
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.explore, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        // TODO: replace with advanced search button
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        // TODO: switch views
+        if (id == R.id.nav_explore) {
+            // Do nothing...
+        } else if (id == R.id.nav_mybucket) {
+            // TODO : Navigation - use fragments or activities?
+            Intent intent = new Intent(Explore.this, MyBucket.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_myhistory) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_about) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -73,7 +149,7 @@ public class Explore extends AppCompatActivity {
 
             TextView tv = (TextView) convertView.findViewById(android.R.id.text1);
 
-            tv.setText(titles[position % 5]);
+            tv.setText(suggestions[position % 5]);
 
             return convertView;
         }
@@ -107,9 +183,9 @@ public class Explore extends AppCompatActivity {
         protected void onPostExecute(MovieDb[] movies) {
 
             for(int i=0; i<5; i++)
-                titles[i] = movies[i].getTitle();
+                suggestions[i] = movies[i].getTitle();
 
-            liste.setAdapter(new SimpleListAdapter());
+            suggestionsList.setAdapter(new SimpleListAdapter());
         }
     }
 }
