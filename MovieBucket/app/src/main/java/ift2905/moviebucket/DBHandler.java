94dce093bddcaa516c8d274ddb22759a420809e4
Merst <noxlittner@gmail.com>
2017-04-09 10:50:32 -0400

    Added a new Class: DBHandler

---
+++ MovieBucket/app/src/main/java/ift2905/moviebucket/DBHandler.java
@@ -0,0 +1,10 @@
+package ift2905.moviebucket;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by XPSFR on 09/04/2017.
+ */
+
+public class DBHandler extends SQLiteOpenHelper {
+}
