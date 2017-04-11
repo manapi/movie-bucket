a3feed3c2ec6a1f5ddf09f79b56f21a12ea5eedd
adeleaubin <adele.aub@gmail.com>
2017-04-11 17:44:32 -0400

    MovieView: year, run time, ratin, genres, overview

--- MovieBucket/app/src/main/java/ift2905/moviebucket/MovieView.java
+++ MovieBucket/app/src/main/java/ift2905/moviebucket/MovieView.java
@@ -10,8 +10,12 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
+import java.util.ListIterator;
+
 import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies;
+import info.movito.themoviedbapi.model.Genre;
 import info.movito.themoviedbapi.model.MovieDb;
 
 
@@ -22,7 +26,7 @@ public class MovieView extends AppCompatActivity {
     final String API_KEY = "93928f442ab5ac81f8c03b874f78fb94";
     final String LANG = "en";
     final String BASE_URL = "http://image.tmdb.org/t/p/";
-    final String SIZE_SMALL = "w342";
+    final String SIZE_MEDIUM = "w342";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,16 +60,89 @@ public class MovieView extends AppCompatActivity {
         @Override
         protected void onPostExecute(MovieDb movie) {
 
-            setTitle(movie.getTitle());
+            // Title
+            TextView titleView = (TextView) findViewById(R.id.movieTitle);
+            try {
+                setTitle(movie.getTitle());
+                titleView.setText(movie.getTitle());
+            } catch (Exception e){
+                titleView.setText("Unknown");
+            }
 
-            TextView title = (TextView) findViewById(R.id.movieTitle);
-            title.setText(movie.getTitle());
+
+            // Year
+            TextView yearView = (TextView) findViewById(R.id.movieYear);
+            try {
+                yearView.setText(movie.getReleaseDate().substring(0,4));
+            } catch (Exception e) {
+                yearView.setText("Unknown");
+            }
+
+
+            // Genres
+            TextView genresView = (TextView) findViewById(R.id.movieGenres);
+            try {
+                List<Genre> listGenres = movie.getGenres();
+                ListIterator genreIterator = listGenres.listIterator();
+                String genres = "";
+                while(genreIterator.hasNext()){
+                    Genre g = (Genre)genreIterator.next();
+                    genres = genres+g.getName()+"\n";
+                }
+                if (genres.isEmpty()){
+                    genresView.setText("Unknown");
+                } else {
+                    genresView.setText(genres);
+                }
+            } catch (Exception e){
+                genresView.setText("Unknown");
+            }
+
+
+            //Rating
+            TextView ratingView = (TextView) findViewById(R.id.movieRating);
+            try {
+                float rating = movie.getVoteAverage();
+                ratingView.setText(Float.toString(rating)+"/10");
+            } catch (Exception e){
+                ratingView.setText("Unknown");
+            }
+
+
+            // Run time
+            TextView runtimeView = (TextView) findViewById(R.id.movieRuntime);
+            try {
+                long runTime = movie.getRuntime();
+                long hours = runTime/60;
+                long minutes = runTime-(hours*60);
+                String minutesT = Long.toString(minutes);
+                if (minutes<10){
+                    minutesT = "0"+minutesT;
+                }
+                runtimeView.setText(Long.toString(hours)+"h"+minutesT);
+            } catch (Exception e){
+                runtimeView.setText("Unknown");
+            }
+
+
+            // Overview
+            TextView overviewView = (TextView) findViewById(R.id.movieOverview);
+            try{
+                overviewView.setText(movie.getOverview());
+
+            } catch (Exception e){
+                overviewView.setText("Unknown");
+            }
+
+            // Poster
             ImageView image = (ImageView) findViewById(R.id.moviePoster);
-
-
-            Picasso.with(getApplicationContext())
-                    .load(BASE_URL + SIZE_SMALL + movie.getPosterPath())
-                    .into(image);
+            try {
+                Picasso.with(getApplicationContext())
+                        .load(BASE_URL + SIZE_MEDIUM + movie.getPosterPath())
+                        .into(image);
+            } catch (Exception e){
+                // TODO: find default image
+            }
 
 
         };
