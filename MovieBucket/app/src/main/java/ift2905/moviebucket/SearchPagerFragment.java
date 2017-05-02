f4ba991eb67329ad4d76d7cb5e8ef96f4e57f85b
Amelie <ame.lacombe.r@gmail.com>
2017-05-01 23:01:34 -0400

    Fixed search duplicate

--- MovieBucket/app/src/main/java/ift2905/moviebucket/SearchPagerFragment.java
+++ MovieBucket/app/src/main/java/ift2905/moviebucket/SearchPagerFragment.java
@@ -123,10 +123,10 @@ public class SearchPagerFragment extends Fragment {
      * Fetch all search results from API
      */
     public void search(String query) {
-        new FetchTop().execute(query, "0");
-        new FetchMovies().execute(query, "0");
-        new FetchSeries().execute(query, "0");
-        new FetchPeople().execute(query, "0");
+        new FetchTop().execute(query, "1");
+        new FetchMovies().execute(query, "1");
+        new FetchSeries().execute(query, "1");
+        new FetchPeople().execute(query, "1");
     }
 
     /**
@@ -158,7 +158,7 @@ public class SearchPagerFragment extends Fragment {
         @Override
         protected void onPostExecute(List<Multi> results) {
             if(topAdapter != null) {
-                if(page == 0) {
+                if(page == 1) {
                     topAdapter.getData().clear();
                     if(!results.isEmpty()) {
                         topAdapter.getData().addAll(results);
@@ -197,7 +197,7 @@ public class SearchPagerFragment extends Fragment {
                 try {
                     TmdbApi api = new TmdbApi(API_KEY);
                     TmdbSearch search = api.getSearch();
-                    return search.searchMovie(query, null, lang, adult, 1).getResults();
+                    return search.searchMovie(query, null, lang, adult, page).getResults();
                 }
                 catch (Exception e) {
                     e.printStackTrace();
@@ -209,7 +209,7 @@ public class SearchPagerFragment extends Fragment {
         @Override
         protected void onPostExecute(List<MovieDb> results) {
             if(movieAdapter != null) {
-                if(page == 0) {
+                if(page == 1) {
                     movieAdapter.getData().clear();
                     if(!results.isEmpty()) {
                         movieAdapter.getData().addAll(results);
@@ -248,7 +248,7 @@ public class SearchPagerFragment extends Fragment {
                 try{
                     TmdbApi api = new TmdbApi(API_KEY);
                     TmdbSearch search = api.getSearch();
-                    return search.searchTv(query, lang, 1).getResults();
+                    return search.searchTv(query, lang, page).getResults();
                 }
                 catch (Exception e) {
                     e.printStackTrace();
@@ -260,7 +260,7 @@ public class SearchPagerFragment extends Fragment {
         @Override
         protected void onPostExecute(List<TvSeries> results) {
             if(seriesAdapter != null) {
-                if(page == 0) {
+                if(page == 1) {
                     seriesAdapter.getData().clear();
                     if (!results.isEmpty()) {
                         seriesAdapter.getData().addAll(results);
@@ -299,7 +299,7 @@ public class SearchPagerFragment extends Fragment {
                 try {
                     TmdbApi api = new TmdbApi(API_KEY);
                     TmdbSearch search = api.getSearch();
-                    return search.searchPerson(query, adult, 1).getResults();
+                    return search.searchPerson(query, adult, page).getResults();
                 }
                 catch (Exception e) {
                     e.printStackTrace();
@@ -311,7 +311,7 @@ public class SearchPagerFragment extends Fragment {
         @Override
         protected void onPostExecute(List<Person> results) {
             if(peopleAdapter != null) {
-                if(page == 0) {
+                if(page == 1) {
                     peopleAdapter.getData().clear();
                     if(!results.isEmpty()) {
                         peopleAdapter.getData().addAll(results);
