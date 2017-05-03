b0833f05a5a36cf81892807c85cad17fa3db5886
Merst <noxlittner@gmail.com>
2017-05-03 11:51:30 -0400

    Major Cleanup/Polishing commit

--- MovieBucket/app/src/main/java/ift2905/moviebucket/SettingsFragment.java
+++ MovieBucket/app/src/main/java/ift2905/moviebucket/SettingsFragment.java
@@ -2,7 +2,6 @@ package ift2905.moviebucket;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
@@ -20,21 +19,13 @@ public class SettingsFragment extends PreferenceFragment
         super.onCreate(savedInstanceState);
         dbh = new DBHandler(getActivity());
         addPreferencesFromResource(R.xml.preferences);
-        //TODO: THERE HAS TO BE A BETTER WAY
-        Preference localePref = findPreference(KEY_LOCALE);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        localePref.setSummary(summary(prefs.getString(KEY_LOCALE, "")));
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String k) {
         if(k.equals(KEY_LOCALE)) {
-            //Updates the summary
-            //TODO: probably has to go.
-            Preference localePref = findPreference(KEY_LOCALE);
-            localePref.setSummary(summary(sharedPreferences.getString(KEY_LOCALE, "")));
             dbh.rebuild();
-            //TODO: refresh de Discover fragment here, end my misery Amé!
+            //TODO: refresh the Discover fragment here, end my misery Amé!
 
         } else  if(k.equals(KEY_ADULT_PREF)) {
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
@@ -54,13 +45,5 @@ public class SettingsFragment extends PreferenceFragment
         super.onPause();
         getPreferenceScreen().getSharedPreferences()
                 .unregisterOnSharedPreferenceChangeListener(this);
-    }
-
-    public String summary(String verifier) {
-        if (verifier.equals("en")) {
-            return getString(R.string.locale_en);
-        } else {
-            return getString(R.string.locale_fr);
-        }
     }
 }
